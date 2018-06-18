719dcf94f1d3f4cf90fd5aefae3d06d575777887
郝海亮 <2298763729@qq.com>
2018-06-18 18:24:55 +0800

    fix:09-04 CSS转化为React

---
+++ 09-04/README.md
@@ -0,0 +1,3 @@
+## CSS转化为React
+https://staxmanade.com/CssToReact/  
+把footer.css转化为footer.js中所示的样式
--- 09-04/src/js/components/footer.js
+++ 09-04/src/js/components/footer.js
@@ -1,14 +1,15 @@
 import React from 'react';
 //var footerCss  =  require("../../css/footer.css");
+
 export default class ComponentFooter extends React.Component {
 	render() {
 		var footerConvertStyle = {
 			"miniFooter": {
-				"backgroundColor": "#333333",
-				"color": "#ffffff",
-				"paddingLeft": "20px",
-				"paddingTop": "3px",
-				"paddingBottom": "3px"
+				"backgroundColor" : "#333333",
+				"color"           : "#ffffff",
+				"paddingLeft"     : "20px",
+				"paddingTop"      : "3px",
+				"paddingBottom"   : "3px"
 			},
 			"miniFooter_h1": {
 				"fontSize": "15px"
