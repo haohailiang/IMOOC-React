c09af5645899356caeb560530823cb20eb0944ed
郝海亮 <2298763729@qq.com>
2018-06-18 18:12:42 +0800

    fix:09-03 CSS组件模块化-不会造成全局污染
    需要增加的插件
    1. react-html-attrs
    2. style-loader
    3. css-loader

---
+++ 09-03/README.md
@@ -0,0 +1,5 @@
+## CSS组件模块化-不会造成全局污染
+需要增加的插件  
+1. react-html-attrs
+2. style-loader
+3. css-loader
--- 09-03/src/js/components/header.js
+++ 09-03/src/js/components/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 export default class ComponentHeader extends React.Component {
 constructor(){
 	super();
