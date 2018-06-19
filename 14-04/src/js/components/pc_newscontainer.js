5dd87aef4eb114a89bc2642f1a2e73c671799994
郝海亮 <2298763729@qq.com>
2018-06-19 11:37:16 +0800

    add:14-04添加轮播器

--- 14-04/src/js/components/pc_newscontainer.js
+++ 14-04/src/js/components/pc_newscontainer.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import {Row, Col} from 'antd';
+import React           from 'react';
+import {Row, Col}      from 'antd';
 import {Tabs,Carousel} from 'antd';
+
 const TabPane = Tabs.TabPane;
+
 export default class PCNewsContainer extends React.Component {
 	render() {
 
     const settings = {
-        dots:true,
-        infinite:true,
-        speed: 500,
-        slidesToShow:1,
-        autoplay:true
+        dots         : true,
+        infinite     : true,
+        speed        : 500,
+        slidesToShow : 1,
+        autoplay     : true
     };
 
 		return (
@@ -18,17 +20,17 @@ export default class PCNewsContainer extends React.Component {
 				<Row>
 					<Col span={2}></Col>
 					<Col span={20} class="container">
-            <div class="leftContainer">
-              <div class="carousel">
-                <Carousel {...settings}>
-                  <div><img src="./src/images/carousel_1.jpg"/></div>
-                  <div><img src="./src/images/carousel_2.jpg"/></div>
-                  <div><img src="./src/images/carousel_3.jpg"/></div>
-                  <div><img src="./src/images/carousel_4.jpg"/></div>
-                </Carousel>
-              </div>
-            </div>
-          </Col>
+						<div class="leftContainer">
+							<div class="carousel">
+								<Carousel {...settings}>
+									<div><img src="./src/images/carousel_1.jpg"/></div>
+									<div><img src="./src/images/carousel_2.jpg"/></div>
+									<div><img src="./src/images/carousel_3.jpg"/></div>
+									<div><img src="./src/images/carousel_4.jpg"/></div>
+								</Carousel>
+							</div>
+						</div>
+					</Col>
 					<Col span={2}></Col>
 				</Row>
 			</div>
