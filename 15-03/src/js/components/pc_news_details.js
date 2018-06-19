3ad3d06e22ac907277fc3d47f78ec0abb5ae12bf
郝海亮 <2298763729@qq.com>
2018-06-19 13:53:03 +0800

    add:15-03添加backtop回到顶部

--- 15-03/src/js/components/pc_news_details.js
+++ 15-03/src/js/components/pc_news_details.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React               from 'react';
 import {Row, Col, BackTop} from 'antd';
-import PCHeader from './pc_header';
-import PCFooter from './pc_footer';
-import PCNewsImageBlock from './pc_news_image_block';
+import PCHeader            from './pc_header';
+import PCFooter            from './pc_footer';
+import PCNewsImageBlock    from './pc_news_image_block';
+
 export default class PCNewsDetails extends React.Component {
 	constructor() {
 		super();
@@ -14,10 +15,12 @@ export default class PCNewsDetails extends React.Component {
 		var myFetchOptions = {
 			method: 'GET'
 		};
-		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
-			this.setState({newsItem: json});
-			document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
-		})
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions)
+			.then(response => response.json())
+			.then(json => {
+				this.setState({newsItem: json});
+				document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
+			})
 	};
 	createMarkup() {
 		return {__html: this.state.newsItem.pagecontent};
