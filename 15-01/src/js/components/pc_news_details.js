269e718fef2b612bb29413b190db5096a840298e
郝海亮 <2298763729@qq.com>
2018-06-19 13:32:50 +0800

    add:新闻列表的详细页面
    1.但是入口还没有写好

--- 15-01/src/js/components/pc_news_details.js
+++ 15-01/src/js/components/pc_news_details.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React      from 'react';
 import {Row, Col} from 'antd';
+
 export default class PCNewsDetails extends React.Component {
 	constructor() {
 		super();
@@ -11,10 +12,12 @@ export default class PCNewsDetails extends React.Component {
 		var myFetchOptions = {
 			method: 'GET'
 		};
-		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
-			this.setState({newsItem: json});
-			document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
-		})
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions)
+			.then(response => response.json())
+			.then(json => {
+				this.setState({newsItem: json});
+				document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
+			})
 	};
 	createMarkup() {
 		return {__html: this.state.newsItem.pagecontent};
