bdc86be5f3892e82e857ee9a1af0da617a09aa53
郝海亮 <2298763729@qq.com>
2018-06-19 13:47:36 +0800

    add:15-02添加路由-链接新闻详情

--- 15-02/src/js/components/pc_news_details.js
+++ 15-02/src/js/components/pc_news_details.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import {Row, Col} from 'antd';
+import React            from 'react';
+import {Row, Col}       from 'antd';
+import PCHeader         from './pc_header';
+import PCFooter         from './pc_footer';
+import PCNewsImageBlock from './pc_news_image_block';
+
 export default class PCNewsDetails extends React.Component {
 	constructor() {
 		super();
@@ -11,10 +15,13 @@ export default class PCNewsDetails extends React.Component {
 		var myFetchOptions = {
 			method: 'GET'
 		};
-		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
-			this.setState({newsItem: json});
-			document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
-		})
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions)
+			.then(response => response.json())
+			.then(json => {
+				console.log( json );
+				this.setState({newsItem: json});
+				document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
+			})
 	};
 	createMarkup() {
 		return {__html: this.state.newsItem.pagecontent};
@@ -22,14 +29,18 @@ export default class PCNewsDetails extends React.Component {
 	render() {
 		return (
 			<div>
+				<PCHeader></PCHeader>
 				<Row>
 					<Col span={2}></Col>
 					<Col span={14} className="container">
 						<div class="articleContainer" dangerouslySetInnerHTML={this.createMarkup()}></div>
 					</Col>
-					<Col span={6}></Col>
+					<Col span={6}>
+						<PCNewsImageBlock count={40} type="top" width="100%" cartTitle="相关新闻" imageWidth="150px" />
+					</Col>
 					<Col span={2}></Col>
 				</Row>
+				<PCFooter></PCFooter>
 			</div>
 		);
 	};
--- 15-02/src/js/root.js
+++ 15-02/src/js/root.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React                        from 'react';
+import ReactDOM                     from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
-import {Button} from 'antd';
-import PCIndex from './components/pc_index';
-import PCNewsDetails from './components/pc_news_details';
-import MobileIndex from './components/mobile_index';
+import {Button}                     from 'antd';
+import PCIndex                      from './components/pc_index';
+import PCNewsDetails                from './components/pc_news_details';
+import MobileIndex                  from './components/mobile_index';
+import MediaQuery                   from 'react-responsive';
+
 import 'antd/dist/antd.css';
-import MediaQuery from 'react-responsive';
+
 export default class Root extends React.Component {
 	render() {
 		return (
