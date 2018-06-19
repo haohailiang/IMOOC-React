27b0cb8240e359672791370fb8fddc0d5f62a8ce
郝海亮 <2298763729@qq.com>
2018-06-19 14:00:27 +0800

    add:15-04添加移动端新闻详情

--- 15-04/src/js/components/mobile_news_details.js
+++ 15-04/src/js/components/mobile_news_details.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React               from 'react';
 import {Row, Col, BackTop} from 'antd';
-import MobileHeader from './mobile_header';
-import MobileFooter from './mobile_footer';
+import MobileHeader        from './mobile_header';
+import MobileFooter        from './mobile_footer';
+
 export default class MobileNewsDetails extends React.Component {
 	constructor() {
 		super();
@@ -13,10 +14,12 @@ export default class MobileNewsDetails extends React.Component {
 		var myFetchOptions = {
 			method: 'GET'
 		};
-		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
-			this.setState({newsItem: json});
-			document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
-		})
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions)
+			.then(response => response.json())
+			.then(json => {
+				this.setState({newsItem: json});
+				document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
+			})
 	};
 	createMarkup() {
 		return {__html: this.state.newsItem.pagecontent};
