b9f60deb4f8b0482e7a7623b2931f1345146e5be
郝海亮 <2298763729@qq.com>
2018-06-23 12:41:30 +0800

    fix:15-06评论功能添加-展示
    1.添加评论
    2.提交评论
    3.评论提交成功

--- 15-06/src/js/components/common_comments.js
+++ 15-06/src/js/components/common_comments.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import {Row, Col} from 'antd';
+import React                                 from 'react';
+import {Row, Col}                            from 'antd';
+import {Router, Route, Link, browserHistory} from 'react-router'
+
 import {
 	Menu,
 	Icon,
@@ -9,59 +11,79 @@ import {
 	Input,
 	Button,
 	CheckBox,
-	Modal
+	Modal,
+	Card
 } from 'antd';
-const FormItem = Form.Item;
-const SubMenu = Menu.SubMenu;
-const TabPane = Tabs.TabPane;
+
+const FormItem      = Form.Item;
+const SubMenu       = Menu.SubMenu;
+const TabPane       = Tabs.TabPane;
 const MenuItemGroup = Menu.ItemGroup;
-import {Router, Route, Link, browserHistory} from 'react-router'
+
 class CommonComments extends React.Component {
 	constructor() {
 		super();
 		this.state = {
 			comments: ''
 		};
+	};
 
-		componentDidMount() {
-			var myFetchOptions = {
-				method: 'GET'
-			};
-			fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getcomments&uniquekey=" + this.props.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
+	componentDidMount() {
+		var myFetchOptions = {
+			method: 'GET'
+		};
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getcomments&uniquekey=" + this.props.uniquekey, myFetchOptions)
+			.then(response => response.json())
+			.then(json => {
 				this.setState({comments: json});
 			})
-		};
+	};
 
 
-		handleSubmit() {};
-		render() {
-			let {getFieldProps} = this.props.form;
-			const {commnets} = this.state;
-			const commnetList = commnets.length?
-			commnets.map((comment,index)=>(
-				<Card key={index} title={comment.UserName} extra={<a href="#">发布于 {commnet.datetime}</a>}>
-					<p>{comment.Commnets}</p>
-				</Card>
-			))
-			:
-			'没有加载到任何评论';
-			return (
-				<div class="comment">
-					<Row>
-						<Col span={24}>
-						{commnetList}
-							<Form onSubmit ={this.handleSubmit.bind(this)}>
-								<FormItem label="您的评论">
-									<Input type="textarea" placeholder="随便写" {...getFieldProps('remark',{initialValue: ''})}/>
-								</FormItem>
-								<Button type="primary" htmlType="submit">提交评论</Button>
-							</Form>
-						</Col>
-					</Row>
-				</div>
-			);
+	handleSubmit(e) {
+		e.preventDefault();
+		var myFetchOptions = {
+			method: 'GET'
 		};
-	}
+
+		var formdata = this.props.form.getFieldsValue();
+
+		debugger;
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=comment&userid=" + localStorage.userid + "&uniquekey=" + this.props.uniquekey + "&commnet=" + formdata.remark, myFetchOptions)
+			.then(response => response.json())
+			.then(json => {
+				this.componentDidMount();
+			})
+	};
+
+	render() {
+		let {getFieldProps} = this.props.form;
+		const {comments}    = this.state;
+
+		const commnetList = comments.length?
+		comments.map((comment,index)=>(
+			<Card key={index} title={comment.UserName} extra={<a href="#">发布于 {comment.datetime}</a>}>
+				<p>{comment.Commnets}</p>
+			</Card>
+		))
+		:
+		'没有加载到任何评论';
+		return (
+			<div class="comment">
+				<Row>
+					<Col span={24}>
+					{commnetList}
+						<Form onSubmit ={this.handleSubmit.bind(this)}>
+							<FormItem label="您的评论">
+								<Input type="textarea" placeholder="随便写" {...getFieldProps('remark',{initialValue: ''})}></Input>
+							</FormItem>
+							<Button type="primary" htmlType="submit">提交评论</Button>
+						</Form>
+					</Col>
+				</Row>
+			</div>
+		);
+	};
 }
 
 export default CommonComments = Form.create({})(CommonComments);
